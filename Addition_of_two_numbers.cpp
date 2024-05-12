dad1e899a30944e1a043c75de25ee9c61e32fb90
Salam2004 <144914772+Salam2004@users.noreply.github.com>
2024-05-12 14:21:23 +0530

    Create Addition_of_two_numbers.cpp

---
+++ Addition_of_two_numbers.cpp
@@ -0,0 +1,18 @@
+class Solution{   
+public:
+    int addition(int A, int B){
+        // code here 
+        return A+B;
+    }
+};
+//This C++ class defines a function
+//`addition` which takes two integer
+//parameters, `A` and `B`, and returns
+//their sum. Inside the function, it 
+//simply adds `A` and `B` together and
+//returns the result. This function can
+//be used to perform addition operations 
+//on two integers by creating an instance
+//of the `Solution` class and calling its
+//`addition` method with the integers you 
+//want to add.
