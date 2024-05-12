685aaae40077287c5afd92c544ad738cb7b25b38
Salam2004 <144914772+Salam2004@users.noreply.github.com>
2024-05-12 14:41:46 +0530

    Create The_dice_problem.cpp

---
+++ The_dice_problem.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int oppositeFaceOfDice(int N)
+    {
+        // Write Your Code here
+        return 7-N;
+    }
+};
+//This C++ class `Solution` contains
+//a function `oppositeFaceOfDice` that
+//calculates the opposite face of a 
+//standard six-sided dice given the face 
+//number `N`. In a standard dice, the sum
+//of opposite faces is always 7. So, to 
+//find the opposite face, the function 
+//subtracts the given face number `N` from 
+//7 and returns the result.
